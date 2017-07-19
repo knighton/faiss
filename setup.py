0d1881adc7ae9b9764861f984deab549216abdcd
James Knighton <iamknighton@gmail.com>
2017-07-19 10:21:49 -0700

    First attempt at a setup.py.

---
+++ setup.py
@@ -0,0 +1,43 @@
+from distutils.command.install import install as DistutilsInstall
+from distutils.sysconfig import get_python_inc
+import numpy
+import os
+from setuptools import setup
+
+
+def find_openblas_so(root_dir):
+    for parent, dirs, files in os.walk(root_dir):
+        for f in files:
+            if f == 'libopenblas.so.0':
+                return os.path.join(root_dir, parent, f)
+
+
+def fill_in_vars():
+    numpy_include = numpy.get_include()
+    openblas_so = find_openblas_so('/usr')
+    python_include = get_python_inc()
+    d = {
+        'numpy_include': numpy_include,
+        'openblas_so': openblas_so,
+        'python_include': python_include,
+    }
+    text = open('makefile.inc.template').read()
+    text = text.format(**d)
+    open('makefile.inc', 'wb').write(text.encode('utf-8'))
+
+
+class MyInstall(DistutilsInstall):
+    def run(self):
+        fill_in_vars()
+        os.system('make -j')
+        os.system('make -j py')
+        os.system('mkdir -p faiss')
+        os.system('mv *.py *.so faiss/')
+        DistutilsInstall.run(self)
+
+
+setup(
+    name='faiss',
+    version='0.0.1',
+    cmdclass={'install': MyInstall},
+)
